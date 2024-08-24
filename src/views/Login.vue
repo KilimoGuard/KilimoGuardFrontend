26eb4a794ffd44ce46f0f6cefc88e7dc5ae416c7
NeemaRhonnahMkenda <nmkenda98@gmail.com>
2024-08-24 16:37:09 +0300

    adjusted login styling to allow user to go back to home page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,33 +1,36 @@
 <template>
   <div class="min-h-screen bg-cover bg-center bg-no-repeat flex items-center justify-center"
        :style="{ backgroundImage: `url(${backgroundImage})` }">
-    <div class="w-full max-w-md bg-white shadow-lg rounded-lg p-8">
-      <h2 class="text-2xl font-bold text-center mb-6">Login</h2>
-      <form @submit.prevent="handleLogin">
-        <div class="mb-4">
-          <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
-          <input type="email" id="email" v-model="email"
-                 class="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-dark-green focus:border-dark-green"
-                 required>
-        </div>
-        <div class="mb-6">
-          <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
-          <input type="password" id="password" v-model="password"
-                 class="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-dark-green focus:border-dark-green"
-                 required>
-        </div>
-        <button type="submit" class="w-full bg-dark-green text-white p-2 rounded-lg hover:bg-green-700">Login</button>
-      </form>
-      <p class="mt-4 text-center text-sm text-gray-600">
-        Don't have an account?
-        <router-link to="/signup" class="text-dark-green hover:underline">Sign up</router-link>
-      </p>
+    <div class="w-full max-w-md bg-white shadow-lg rounded-lg">
+      <p class="text-xs sticky top-0 mt-4 ml-4 text-blue-500 hover:text-blue-700"><a href="/"><i class="bi bi-arrow-left text-2xs mr-1"></i> return to home</a></p>
+      <div class="pl-8 pr-8 pb-8 mt-2">
+        <h2 class="text-2xl font-bold text-center mb-6">Login</h2>
+        <form @submit.prevent="handleLogin">
+          <div class="mb-4">
+            <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+            <input type="email" id="email" v-model="email"
+                   class="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-dark-green focus:border-dark-green"
+                   required>
+          </div>
+          <div class="mb-6">
+            <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
+            <input type="password" id="password" v-model="password"
+                   class="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-dark-green focus:border-dark-green"
+                   required>
+          </div>
+          <button type="submit" class="w-full bg-dark-green text-white p-2 rounded-lg hover:bg-green-700">Login</button>
+        </form>
+        <p class="mt-4 text-center text-sm text-gray-600">
+          Don't have an account?
+          <router-link to="/signup" class="text-dark-green hover:underline">Sign up</router-link>
+        </p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { useAuthStore } from '@/stores/auth';
+import {useAuthStore} from '@/stores/auth';
 
 export default {
   name: 'LogIn',
